5e2f7fd9fad41abe3125189fa3cfef66a50185c0
Barnold88 <bandslover@outlook.com>
2025-01-14 23:17:26 +0000

    Add CSS styling to clickable element

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,63 @@
 hidden_elements = document.getElementsByClassName('reveal-information')
 counter = 0
 
+buttonClass = document.createElement('style')
+buttonClass.type = "text/css"
+buttonClass.innerHTML = `.button-3 {
+    appearance: none;
+    background-color: #2ea44f;
+    border: 1px solid rgba(27, 31, 35, .15);
+    border-radius: 6px;
+    box-shadow: rgba(27, 31, 35, .1) 0 1px 0;
+    box-sizing: border-box;
+    color: #fff;
+    cursor: pointer;
+    display: inline-block;
+    font-family: -apple-system,system-ui,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji";
+    font-size: 14px;
+    font-weight: 600;
+    line-height: 20px;
+    padding: 6px 16px;
+    position: relative;
+    text-align: center;
+    text-decoration: none;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    vertical-align: middle;
+    white-space: nowrap;
+  }
+  
+  .button-3:focus:not(:focus-visible):not(.focus-visible) {
+    box-shadow: none;
+    outline: none;
+  }
+  
+  .button-3:hover {
+    background-color: #2c974b;
+  }
+  
+  .button-3:focus {
+    box-shadow: rgba(46, 164, 79, .4) 0 0 0 3px;
+    outline: none;
+  }
+  
+  .button-3:disabled {
+    background-color: #94d3a2;
+    border-color: rgba(27, 31, 35, .1);
+    color: rgba(255, 255, 255, .8);
+    cursor: default;
+  }
+  
+  .button-3:active {
+    background-color: #298e46;
+    box-shadow: rgba(20, 70, 32, .2) 0 1px 0 inset;
+  } `
+
+
+document.getElementsByTagName('head')[0].appendChild(buttonClass)
+
+
 function getDate(){
 
     const date = new Date();
@@ -16,7 +73,7 @@ for (let item of hidden_elements) {
 
     button = document.createElement("div") // creating a button in the loop allows each element to have a button? what
     button.innerText = "Add today's date"
-
+    button.className = "button-3"
     button.onclick = function(e){
 
         date = getDate()
@@ -40,3 +97,5 @@ for (let item of hidden_elements) {
 
     item.appendChild(button)
 }
+
+
